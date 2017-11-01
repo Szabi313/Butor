0c0301b265f38a2c7ec823f479cea506d3c24594
Szabolcs Torma <szabolcs_torma@epam.com>
2017-11-01 12:46:37 +0100

    front js parent finding

--- js/butor-front.js
+++ js/butor-front.js
@@ -219,7 +219,7 @@ $(document).ready(function(){
  
 	$(".submit-button").click(function(e){
 		
-		console.log(this);
+		console.log(this.parent());
 		
 		if(!$("#subscribe-name").val() || !$("#subscribe-email").val())alert("A név és az e-mail mező kitöltése kötelező")
 		else{
