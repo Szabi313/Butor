ecdaa4b90b9e9c7356593c4da2dd62d85ca64d97
Szabolcs Torma <szabolcs_torma@epam.com>
2017-11-01 12:33:44 +0100

    hírlevél küldésgomb class-t kapott

--- application/views/butor/butor_subscribe.php
+++ application/views/butor/butor_subscribe.php
@@ -6,7 +6,7 @@
 			<input id="subscribe-name" name="subscribe-name" style="border-radius:5px; width:100%; height:40px; border: 1px solid; box-sizing:border-box; padding:2px; hint:#ddd" placeholder="név"></label><br>
 			<label for="subscribe-email" style="width:95%">E-mail cím: <br>
 			<input type="email" id="subscribe-email" name="subscribe-email" style="border-radius:5px; width:100%; height:40px; border: 1px solid; padding:2px; hint:#ddd" placeholder="E-mail"></label><br><br>
-			<input type="button" id="submit-button" value="elküld" style="border-radius:5px; width:95%; height:40px; border: 1px solid; box-sizing:border-box;">
+			<input type="button" id="submit-button" class="submit-button" value="elküld" style="border-radius:5px; width:95%; height:40px; border: 1px solid; box-sizing:border-box;">
 		</div>
 		<div id="loading2" style="display:none; position:absolute; width:100%; height:100%; top:0; background-color: rgba(255,255,255,0.5); background-image: url(http://butorstudiogalgaheviz.hu/images/loading.gif); background-size: 100px 100px; background-repeat:  no-repeat; background-position: center center; "></div>
 </div>
