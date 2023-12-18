009c72b34f22fe895a3818823bc3603b05c8419e
Szabi313 <szabi313@gmail.com>
2023-12-18 17:50:20 +0100

    Update config.php to https

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://butorstudiogalgaheviz.hu/';
+$config['base_url'] = 'https://butorstudiogalgaheviz.hu/';
 //$config['base_url'] = 'http://bur/butor-php/';
 
 /*
